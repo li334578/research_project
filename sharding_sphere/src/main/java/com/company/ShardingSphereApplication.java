db46ec73ed3d69d39c16c4a5224691d8671d88b8
LiWenBo <liwenbo4922@163.com>
2021-04-22 21:00:45 +0800

    完善项目结构

---
+++ sharding_sphere/src/main/java/com/company/ShardingSphereApplication.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description : ShardingSphere
+ * @date : 2021-04-21 20:54:18
+ */
+@SpringBootApplication
+public class ShardingSphereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereApplication.class, args);
+    }
+}
---
+++ sharding_sphere/src/test/java/com/company/TestClass1.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.*;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-04-21 21:08:28
+ */
+@SpringBootTest
+@Slf4j
+public class TestClass1 {
+
+    @Test
+    public void testMethod1() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 20, 10,
+                TimeUnit.MICROSECONDS, new LinkedBlockingQueue<Runnable>());
+        Integer sum = 0;
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        for (int i = 1; i < 10000; i += 10) {
+            Future<Integer> future
+                    = threadPoolExecutor.submit(new MyTask1(i, countDownLatch));
+            try {
+                sum += future.get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        log.info("result -> " + sum);
+    }
+}
+
+
+class MyTask1 implements Callable<Integer> {
+
+    private Integer startNum;
+    private CountDownLatch countDownLatch;
+
+    public MyTask1(Integer startNum, CountDownLatch countDownLatch) {
+        this.startNum = startNum;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public Integer call() {
+        System.out.println(Thread.currentThread().getName()
+                + String.format("开始计算%d---->%d", startNum, startNum + 1000));
+        int sum = 0;
+        for (int i = startNum; i < startNum + 10; i++) {
+            sum += i;
+        }
+        countDownLatch.countDown();
+        return sum;
+    }
+}
