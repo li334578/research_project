a7c198f96422203048c6a1c3b6b613bc677652db
LiWenBo <liwenbo4922@163.com>
2021-05-21 21:22:21 +0800

    GC

---
+++ sharding_sphere/src/test/java/com/company/sharding_sphere/other/RefCountGC2.java
@@ -0,0 +1,14 @@
+package com.company.sharding_sphere.other;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-05-11 21:06:15
+ */
+public class RefCountGC2 {
+    public static void main(String[] args) {
+        byte[] data = new byte[6 * 1024 * 1024];
+        
+    }
+}
