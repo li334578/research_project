28c3c0fed88401ed9bd1d8792e4dc0292a1262bb
LiWenBo <liwenbo4922@163.com>
2021-08-01 16:55:50 +0800

    synchronized 锁升级

--- research_spring/src/test/java/com/company/research_spring/ResearchSpringApplicationTests.java
+++ research_spring/src/test/java/com/company/research_spring/ResearchSpringApplicationTests.java
@@ -1,13 +1,24 @@
 package com.company.research_spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.ClassLayout;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ResearchSpringApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testMethod1() {
+        Object o = new Object();
+        log.info(ClassLayout.parseInstance(o).toPrintable());
+        synchronized (o) {
+            log.info(ClassLayout.parseInstance(o).toPrintable());
+        }
+    }
 }
