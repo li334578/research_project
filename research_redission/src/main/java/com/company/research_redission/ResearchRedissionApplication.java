685d9f6d43e8e5716456afb0a4909e1a134c92a6
LiWenBo <liwenbo4922@163.com>
2021-08-02 20:20:39 +0800

    redission

---
+++ research_redission/src/main/java/com/company/research_redission/ResearchRedissionApplication.java
@@ -0,0 +1,13 @@
+package com.company.research_redission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchRedissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchRedissionApplication.class, args);
+    }
+
+}
---
+++ research_redission/src/test/java/com/company/research_redission/ResearchRedissionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.company.research_redission;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class ResearchRedissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
