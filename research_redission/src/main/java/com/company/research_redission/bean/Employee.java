940270731bd39f231e8719338d34dbc19d83bfd3
LiWenBo <liwenbo4922@163.com>
2021-08-04 21:01:42 +0800

    Employee

---
+++ research_redission/src/main/java/com/company/research_redission/bean/Employee.java
@@ -0,0 +1,16 @@
+package com.company.research_redission.bean;
+
+import lombok.Data;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-07-31 21:32:55
+ */
+@Data
+public class Employee {
+    private String name;
+    private Integer age;
+    private String remark;
+}
