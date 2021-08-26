fc391f8c0ff1801d5c589ef8c6223957403f79b6
LiWenBo <liwenbo4922@163.com>
2021-08-26 20:46:45 +0800

    Zookeeper Main

---
+++ research_zookeeper/src/main/java/com/company/research_zookeeper/ResearchZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.company.research_zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchZookeeperApplication.class, args);
+    }
+
+}
