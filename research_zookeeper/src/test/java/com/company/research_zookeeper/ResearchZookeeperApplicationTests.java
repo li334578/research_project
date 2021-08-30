b291c71d9c6b52565e7b8b72c40c52998e770b3e
LiWenBo <liwenbo4922@163.com>
2021-08-30 20:23:25 +0800

    test

---
+++ research_zookeeper/src/test/java/com/company/research_zookeeper/ResearchZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.research_zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchZookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
