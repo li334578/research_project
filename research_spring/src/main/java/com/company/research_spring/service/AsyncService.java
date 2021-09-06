871596f570fdc0bfff2172bd9707bdb892e87220
LiWenBo <liwenbo4922@163.com>
2021-09-06 20:59:35 +0800

    AsyncService

---
+++ research_spring/src/main/java/com/company/research_spring/service/AsyncService.java
@@ -0,0 +1,15 @@
+package com.company.research_spring.service;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-09-04 10:02:39
+ */
+public interface AsyncService {
+
+    /**
+     * 执行异步任务 * 可以根据需求，自己加参数拟定
+     */
+    void executeAsync();
+}
