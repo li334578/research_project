4f601f6ecbf2bba40e505c410b3ad89883210a33
LiWenBo <liwenbo4922@163.com>
2021-10-18 20:34:19 +0800

    micro_service_1

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/MicroService1Application.java
@@ -0,0 +1,15 @@
+package com.company.micro_service_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroService1Application.class, args);
+    }
+
+}
---
+++ micro_service_1/src/test/java/com/company/micro_service_1/MicroService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.micro_service_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
