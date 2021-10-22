4784c0f373eb929777b502d2f2088299c22d1ab0
LiWenBo <liwenbo4922@163.com>
2021-10-22 20:38:36 +0800

    StudentController

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.company.micro_service_1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-10-22 20:36:53
+ */
+@RestController
+public class StudentController {
+}
