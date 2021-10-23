1cf300d716f1b04a8ed1f4560fac8c9d771626d2
LiWenBo <liwenbo4922@163.com>
2021-10-23 20:41:15 +0800

    Student

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/StudentController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.company.micro_service_1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date : 2021-10-22 20:36:53
  */
 @RestController
+@RequestMapping("/student")
 public class StudentController {
+
+
+    @GetMapping("/")
+    public void student(){
+
+    }
 }
