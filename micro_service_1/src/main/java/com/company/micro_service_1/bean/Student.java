d241d28fad4914eecb32cd2c8a5bfdfd4b463fdd
LiWenBo <liwenbo4922@163.com>
2021-10-26 20:26:34 +0800

    Student

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/bean/Student.java
@@ -0,0 +1,18 @@
+package com.company.micro_service_1.bean;
+
+import lombok.Data;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-10-24 19:56:10
+ */
+@Data
+public class Student {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
