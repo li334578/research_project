708993a71fe52d86e927ebca5e7ebe6416d8f496
LiWenBo <liwenbo4922@163.com>
2021-10-27 20:12:39 +0800

    StudentMapper

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/mapper/StudentMapper.java
@@ -0,0 +1,14 @@
+package com.company.micro_service_1.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.company.micro_service_1.bean.Student;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-10-24 19:57:34
+ */
+public interface StudentMapper extends BaseMapper<Student> {
+
+}
