ea7d519e36518bc7e7efb86c59b4f66db63a20e4
LiWenBo <liwenbo4922@163.com>
2021-10-28 20:21:02 +0800

    StudentService

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.company.micro_service_1.service;
+
+import com.company.micro_service_1.bean.Student;
+
+import java.util.List;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-10-24 19:56:32
+ */
+public interface StudentService {
+
+    List<Student> students();
+}
