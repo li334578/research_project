5f09aa5db1ee8d8c6ee3eca207616e45ed6df14f
LiWenBo <liwenbo4922@163.com>
2021-10-29 20:21:31 +0800

    StudentServiceImpl

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/impl/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.company.micro_service_1.service.impl;
+
+import com.company.micro_service_1.bean.Student;
+import com.company.micro_service_1.mapper.StudentMapper;
+import com.company.micro_service_1.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-10-24 19:56:49
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Resource
+    private StudentMapper studentMapper;
+
+    @Override
+    public List<Student> students() {
+        return studentMapper.selectList(null);
+    }
+}
