d5669f60604dbe4b777bee807415c46cf6bfc669
LiWenBo <liwenbo4922@163.com>
2021-11-06 08:46:36 +0800

    studentDto

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/dto/StudentDto.java
@@ -0,0 +1,13 @@
+package com.company.micro_service_1.dto;
+
+import com.company.micro_service_1.bean.Student;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-11-04 20:26:49
+ */
+public class StudentDto extends Student {
+
+}
