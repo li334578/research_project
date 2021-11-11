308530438a04f46a4502b47e66b6ca0302d0e6ee
LiWenBo <liwenbo4922@163.com>
2021-11-11 20:19:57 +0800

    StudentDto

--- micro_service_1/src/main/java/com/company/micro_service_1/dto/StudentDto.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/dto/StudentDto.java
@@ -1,6 +1,8 @@
 package com.company.micro_service_1.dto;
 
 import com.company.micro_service_1.bean.Student;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author : LiWenBo
@@ -8,6 +10,9 @@ import com.company.micro_service_1.bean.Student;
  * @description :
  * @date : 2021-11-04 20:26:49
  */
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class StudentDto extends Student {
 
 }
