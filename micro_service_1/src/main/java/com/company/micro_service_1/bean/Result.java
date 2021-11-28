c226945a53ab6bcb8b7bc7d50ed6b6f111f70acc
LiWenBo <liwenbo4922@163.com>
2021-11-28 19:28:08 +0800

    Result

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/bean/Result.java
@@ -0,0 +1,43 @@
+package com.company.micro_service_1.bean;
+
+import lombok.Data;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-11-26 20:22:02
+ */
+@Data
+public class Result<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public Result() {
+    }
+
+    public Result(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    /**
+     * 成功
+     */
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<T>();
+        result.setCode(ResultMsgEnum.SUCCESS.getCode());
+        result.setMessage(ResultMsgEnum.SUCCESS.getMessage());
+        result.setData(data);
+        return result;
+    }
+
+    /**
+     * 失败
+     */
+    public static <T> Result<T> error(int code, String message) {
+        return new Result(code, message);
+    }
+}
