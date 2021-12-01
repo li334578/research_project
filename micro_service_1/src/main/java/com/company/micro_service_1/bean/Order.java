df34ca9fd0e038a899638552fd3c04d1ed57c9e5
LiWenBo <liwenbo4922@163.com>
2021-12-01 20:19:02 +0800

    Order

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/bean/Order.java
@@ -0,0 +1,10 @@
+package com.company.micro_service_1.bean;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-12-01 20:18:06
+ */
+public class Order {
+}
