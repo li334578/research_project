e0ec26d9d2a106c6b60440342eb7f884e971bf80
LiWenBo <liwenbo4922@163.com>
2021-12-02 20:15:16 +0800

    OrederMapper

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/mapper/OrederMapper.java
@@ -0,0 +1,10 @@
+package com.company.micro_service_1.mapper;
+
+/**
+ * @author : LiWenBo
+ * @program : research_project
+ * @description :
+ * @date : 2021-12-02 20:14:53
+ */
+public interface OrederMapper {
+}
