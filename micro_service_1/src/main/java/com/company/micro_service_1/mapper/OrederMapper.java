a04ea6eae30597ead472d43a6f480c089c44f0ab
LiWenBo <liwenbo4922@163.com>
2021-12-03 19:47:57 +0800

    extends

--- micro_service_1/src/main/java/com/company/micro_service_1/mapper/OrederMapper.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/mapper/OrederMapper.java
@@ -1,10 +1,13 @@
 package com.company.micro_service_1.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.company.micro_service_1.bean.Order;
+
 /**
  * @author : LiWenBo
  * @program : research_project
  * @description :
  * @date : 2021-12-02 20:14:53
  */
-public interface OrederMapper {
+public interface OrederMapper extends BaseMapper<Order> {
 }
