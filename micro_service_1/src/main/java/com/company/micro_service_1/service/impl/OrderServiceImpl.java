f1b57054e9dab0d3b16d56ebac1ffd951f442c6b
liwenbo <liwenbo4922@163.com>
2021-12-06 19:56:27 +0800

    OrderServiceImpl

--- micro_service_1/src/main/java/com/company/micro_service_1/service/impl/OrderServiceImpl.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/impl/OrderServiceImpl.java
@@ -1,4 +1,6 @@
 package com.company.micro_service_1.service.impl;
 
-public class OrderServiceImpl {
+import com.company.micro_service_1.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
 }
