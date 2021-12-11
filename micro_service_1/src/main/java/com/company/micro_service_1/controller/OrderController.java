c41d8e05c47b910f9118d93ecf517c153422f569
liwenbo <liwenbo4922@163.com>
2021-12-11 19:57:15 +0800

    OrderController

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/OrderController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/OrderController.java
@@ -1,7 +1,13 @@
 package com.company.micro_service_1.controller;
 
+import com.company.micro_service_1.service.OrderService;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class OrderController {
+
+    @Resource
+    private OrderService orderService;
 }
