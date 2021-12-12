e084ea67bcf34f03a0e3af9c11751d6080a8188b
liwenbo <liwenbo4922@163.com>
2021-12-12 19:47:30 +0800

    OrderController

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/OrderController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.company.micro_service_1.controller;
 
 import com.company.micro_service_1.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
     @Resource
