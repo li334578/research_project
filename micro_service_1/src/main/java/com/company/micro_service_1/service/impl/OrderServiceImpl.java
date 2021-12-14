a2c5ea042ed673cb2b1c8d277c30537927fc6038
liwenbo <liwenbo4922@163.com>
2021-12-14 19:46:15 +0800

    OrderServiceImpl

--- micro_service_1/src/main/java/com/company/micro_service_1/service/impl/OrderServiceImpl.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/impl/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.company.micro_service_1.bean.Order;
 import com.company.micro_service_1.mapper.OrderMapper;
 import com.company.micro_service_1.service.OrderService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements OrderService {
 }
