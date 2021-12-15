d9feb9332136d2a23389626f7e46d1ecaa878e8a
liwenbo <liwenbo4922@163.com>
2021-12-15 20:25:56 +0800

    OrderController

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/OrderController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/OrderController.java
@@ -1,11 +1,14 @@
 package com.company.micro_service_1.controller;
 
+import com.company.micro_service_1.bean.Order;
 import com.company.micro_service_1.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -14,4 +17,10 @@ public class OrderController {
 
     @Resource
     private OrderService orderService;
+
+    @GetMapping("/")
+    public void getOrderList() {
+        List<Order> list = orderService.list();
+        list.forEach(System.out::println);
+    }
 }
