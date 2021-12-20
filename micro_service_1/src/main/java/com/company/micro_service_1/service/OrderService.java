28cd84c000ff948b6b0d98f8fb0ab2fc88daf82e
liwenbo <liwenbo4922@163.com>
2021-12-20 20:01:47 +0800

    OrderController

--- micro_service_1/src/main/java/com/company/micro_service_1/service/OrderService.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.company.micro_service_1.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.company.micro_service_1.bean.Order;
 
 public interface OrderService extends IService<Order> {
+
+    void delete(QueryWrapper<Order> queryWrapper);
 }
