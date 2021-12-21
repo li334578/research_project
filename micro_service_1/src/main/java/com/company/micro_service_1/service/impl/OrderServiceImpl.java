cb5cb58054f2b6b8b59af70ef6a10733c2565132
liwenbo <liwenbo4922@163.com>
2021-12-21 20:04:01 +0800

    OrderController

--- micro_service_1/src/main/java/com/company/micro_service_1/service/impl/OrderServiceImpl.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/impl/OrderServiceImpl.java
@@ -1,11 +1,22 @@
 package com.company.micro_service_1.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.company.micro_service_1.bean.Order;
 import com.company.micro_service_1.mapper.OrderMapper;
 import com.company.micro_service_1.service.OrderService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements OrderService {
+
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Override
+    public void delete(QueryWrapper<Order> queryWrapper) {
+        orderMapper.delete(queryWrapper);
+    }
 }
