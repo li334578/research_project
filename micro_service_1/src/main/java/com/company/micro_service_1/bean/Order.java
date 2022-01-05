14d6f6a8af85b1f50a34ecff4b01d8833eebfa0c
liwenbo <liwenbo4922@163.com>
2022-01-05 15:43:03 +0800

    Order

--- micro_service_1/src/main/java/com/company/micro_service_1/bean/Order.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/bean/Order.java
@@ -1,10 +1,14 @@
 package com.company.micro_service_1.bean;
 
+import lombok.Data;
+
 /**
  * @author : LiWenBo
  * @program : research_project
  * @description :
  * @date : 2021-12-01 20:18:06
  */
+@Data
 public class Order {
+    private Long id;
 }
