7ccb453e14f491d877ab12b3683b972311effae7
liwenbo <liwenbo4922@163.com>
2022-01-24 09:35:23 +0800

    exist

--- micro_service_1/src/main/java/com/company/micro_service_1/util/EsUtil.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/util/EsUtil.java
@@ -2,9 +2,11 @@ package com.company.micro_service_1.util;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.transport.endpoints.BooleanResponse;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @Component
 public class EsUtil<T> {
@@ -13,4 +15,23 @@ public class EsUtil<T> {
 
     @Resource
     private ElasticsearchAsyncClient elasticsearchAsyncClient;
+
+
+    /**
+     * 判断索引是否存在
+     *
+     * @param indexName 索引名称
+     * @return 是否存在
+     */
+    private Boolean exist(String indexName) {
+        try {
+            BooleanResponse existsResponse = elasticsearchClient.indices()
+                    .exists(c -> c.index(indexName));
+            // 存在返回true 不存在返回false
+            return existsResponse.value();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
