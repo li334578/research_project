1781b33be5161783052d204872f5f9ed67bbb179
liwenbo <liwenbo4922@163.com>
2022-01-25 09:13:54 +0800

    createIndex

--- micro_service_1/src/main/java/com/company/micro_service_1/util/EsUtil.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/util/EsUtil.java
@@ -2,6 +2,7 @@ package com.company.micro_service_1.util;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import co.elastic.clients.transport.endpoints.BooleanResponse;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,29 @@ public class EsUtil<T> {
 
     @Resource
     private ElasticsearchAsyncClient elasticsearchAsyncClient;
+
+    /**
+     * 创建索引 不指定mapping
+     *
+     * @param indexName 索引名称
+     * @return 是否创建成功
+     */
+    public Boolean createIndex(String indexName) {
+        // 检查索引是否存在
+        try {
+            if (exist(indexName)) {
+                return false;
+            }
+            // 创建索引
+            CreateIndexResponse response = elasticsearchClient.indices().create(c -> c.index(indexName));
+            return response.acknowledged();
+        } catch (IOException e) {
+            // 访问es失败 返回false
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 
 
     /**
