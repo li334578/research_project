84f28e4da5da190508a6d1ceedda40bd22abb7cd
liwenbo <liwenbo4922@163.com>
2022-02-28 16:18:29 +0800

    testMethod1

--- micro_service_1/src/test/java/com/company/micro_service_1/EsTestClass.java
+++ micro_service_1/src/test/java/com/company/micro_service_1/EsTestClass.java
@@ -2,11 +2,14 @@ package com.company.micro_service_1;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import com.company.micro_service_1.util.EsUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @SpringBootTest
 @Slf4j
@@ -19,4 +22,11 @@ public class EsTestClass {
 
     @Resource
     private EsUtil esUtil;
+
+    @Test
+    public void testMethod1() throws IOException {
+        // 创建索引
+        CreateIndexResponse response = elasticsearchClient.indices().create(c -> c.index("product01"));
+        System.out.println(response);
+    }
 }
