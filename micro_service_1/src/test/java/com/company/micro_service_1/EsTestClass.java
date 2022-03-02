cea59bcc2720e5a9a5fd67ed1ee1fa341944aa88
liwenbo <liwenbo4922@163.com>
2022-03-02 09:48:39 +0800

    testMethod1_3

--- micro_service_1/src/test/java/com/company/micro_service_1/EsTestClass.java
+++ micro_service_1/src/test/java/com/company/micro_service_1/EsTestClass.java
@@ -2,10 +2,7 @@ package com.company.micro_service_1;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch._types.mapping.IntegerNumberProperty;
-import co.elastic.clients.elasticsearch._types.mapping.KeywordProperty;
-import co.elastic.clients.elasticsearch._types.mapping.Property;
-import co.elastic.clients.elasticsearch._types.mapping.TextProperty;
+import co.elastic.clients.elasticsearch._types.mapping.*;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import com.company.micro_service_1.util.EsUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -48,5 +45,22 @@ public class EsTestClass {
         map.put("remark", textProperty);
         CreateIndexResponse response = elasticsearchClient.indices().create(c -> c.index("product02").mappings(type -> type.properties(map)));
         System.out.println(response);
+    }
+
+    @Test
+    public void testMethod1_3() {
+        // 创建索引
+        Map<String, Property> map = new HashMap<>();
+        Property keywordProperty = new Property(new KeywordProperty.Builder().build());
+        Property integerProperty = new Property(new IntegerNumberProperty.Builder().build());
+        Property textProperty = new Property(new TextProperty.Builder().build());
+        Property doubleProperty = new Property(new DoubleNumberProperty.Builder().build());
+        map.put("id", integerProperty);
+        map.put("name", keywordProperty);
+        map.put("age", integerProperty);
+        map.put("score", doubleProperty);
+        map.put("className", textProperty);
+        map.put("remark", textProperty);
+        esUtil.createIndex("product01", map);
     }
 }
