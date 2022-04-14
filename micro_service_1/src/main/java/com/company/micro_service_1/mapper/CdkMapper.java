912b8feea468c7941e323eae6c69d12a969fa967
liwenbo <liwenbo4922@163.com>
2022-04-14 19:25:11 +0800

    cdkMapper

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/mapper/CdkMapper.java
@@ -0,0 +1,16 @@
+package com.company.micro_service_1.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.company.micro_service_1.bean.Cdk;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Date 4/3/2022 0004 上午 11:27
+ * @Description 兑换码mapper
+ * @Version 1.0.0
+ * @Author liwenbo
+ */
+@Mapper
+public interface CdkMapper extends BaseMapper<Cdk> {
+
+}
