e9386a1dd3e1831e2318bf4bf19b690e3256d1bf
liwenbo <liwenbo4922@163.com>
2022-04-15 20:11:20 +0800

    ReceiveMapper

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/mapper/ReceiveMapper.java
@@ -0,0 +1,15 @@
+package com.company.micro_service_1.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.company.micro_service_1.bean.Receive;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Date 4/3/2022 0004 下午 2:19
+ * @Description TODO
+ * @Version 1.0.0
+ * @Author liwenbo
+ */
+@Mapper
+public interface ReceiveMapper extends BaseMapper<Receive> {
+}
