988110ff7971c81b19516cdb3cdb7dc551ee9d10
liwenbo <liwenbo4922@163.com>
2022-04-17 20:01:30 +0800

    CdkService

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/CdkService.java
@@ -0,0 +1,13 @@
+package com.company.micro_service_1.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.company.micro_service_1.bean.Cdk;
+
+/**
+ * @Date 4/3/2022 0004 上午 11:28
+ * @Description cdk Service
+ * @Version 1.0.0
+ * @Author liwenbo
+ */
+public interface CdkService extends IService<Cdk> {
+}
