dd736e441e471723666300e09a9847d0f9988f17
liwenbo <liwenbo4922@163.com>
2022-04-18 19:15:14 +0800

    ReceiveService

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/ReceiveService.java
@@ -0,0 +1,13 @@
+package com.company.micro_service_1.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.company.micro_service_1.bean.Receive;
+
+/**
+ * @Date 4/3/2022 0004 下午 2:20
+ * @Description TODO
+ * @Version 1.0.0
+ * @Author liwenbo
+ */
+public interface ReceiveService extends IService<Receive> {
+}
