dc27d14ac1f992f999e3029e488575e6202e07ae
liwenbo <liwenbo4922@163.com>
2022-04-20 20:18:03 +0800

    BatchRequestController

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/BatchRequestController.java
@@ -0,0 +1,14 @@
+package com.company.micro_service_1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname BatchRequestController
+ * @Description 批量请求controller
+ * @Version 1.0.0
+ * @Date 20/4/2022 下午 8:17
+ * @Created by 李文博
+ */
+@RestController
+public class BatchRequestController {
+}
