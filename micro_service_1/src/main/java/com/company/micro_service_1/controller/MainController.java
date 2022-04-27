8c810445f08bd7f51be339fda46ab00db277ab35
liwenbo <liwenbo4922@163.com>
2022-04-27 20:07:31 +0800

    MainController

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.company.micro_service_1.controller;
+
+/**
+ * @Classname MainController
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 27/4/2022 下午 8:07
+ * @Created by 李文博
+ */
+public class MainController {
+}
