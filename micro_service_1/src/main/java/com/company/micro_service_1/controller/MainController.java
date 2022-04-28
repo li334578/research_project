7e081e5aff983117422551ca043b4e3db6efcc31
liwenbo <liwenbo4922@163.com>
2022-04-28 22:20:55 +0800

    MainController

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/MainController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.company.micro_service_1.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Classname MainController
  * @Description TODO
@@ -7,5 +11,8 @@ package com.company.micro_service_1.controller;
  * @Date 27/4/2022 下午 8:07
  * @Created by 李文博
  */
+@RestController
+@RequestMapping("/main/")
+@Slf4j
 public class MainController {
 }
