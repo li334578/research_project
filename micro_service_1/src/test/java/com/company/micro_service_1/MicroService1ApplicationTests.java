3f23d65e454b40ee82f0e1fc6a3aff3f5beb6ef3
liwenbo <liwenbo4922@163.com>
2022-05-14 21:45:32 +0800

    join

--- micro_service_1/src/test/java/com/company/micro_service_1/MicroService1ApplicationTests.java
+++ micro_service_1/src/test/java/com/company/micro_service_1/MicroService1ApplicationTests.java
@@ -8,6 +8,10 @@ import org.redisson.api.RBucket;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -34,5 +38,48 @@ class MicroService1ApplicationTests {
         nameRBucket.set("四哥", 30, TimeUnit.SECONDS);
         // 通过key获取value
         System.out.println(redisson.getBucket("name").get());
+    }
+
+    @Test
+    public void testMethod3() {
+        // 理解join例子 三个线程 t1 t2 t3 按照 t3 t2 t1的顺序执行
+        Thread t3 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " start");
+            System.out.println(Thread.currentThread().getName() + " end");
+        }, "t3");
+        Thread t2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " start");
+            System.out.println(Thread.currentThread().getName() + " end");
+        }, "t2");
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " start");
+            System.out.println(Thread.currentThread().getName() + " end");
+        }, "t1");
+
+        try {
+            t3.start();
+            t2.start();
+            t1.start();
+            t3.join();
+            t2.join();
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("请输入数字");
+        Scanner scanner = new Scanner(System.in);
+
+        String inputValue = scanner.nextLine();
+        String[] split = inputValue.split("");
+        Map<Integer, Integer> map = Arrays.stream(split).reduce(new HashMap<>(), (h1, v1) -> {
+            Integer intV1 = Integer.valueOf(v1);
+            Integer nextValue = h1.getOrDefault(intV1, 0) + 1;
+            h1.put(intV1, nextValue);
+            return h1;
+        }, (k1, k2) -> k1);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ":" + map.getOrDefault(i, 0));
+        }
     }
 }
