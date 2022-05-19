034f460280a2a8424f8d42eda7a53197526ede8c
liwenbo <liwenbo4922@163.com>
2022-05-19 20:53:43 +0800

    criticalValue

--- micro_service_1/src/main/java/com/company/micro_service_1/bean/MyBlockingQueue.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/bean/MyBlockingQueue.java
@@ -13,4 +13,14 @@ import java.util.concurrent.LinkedBlockingDeque;
  */
 @Slf4j
 public class MyBlockingQueue <R extends Runnable> extends LinkedBlockingDeque<Runnable> {
+
+
+    /**
+     * 队列中堆积的任务超过该值后 会创建非核心线程执行任务用于降低系统负载
+     */
+    private Integer criticalValue;
+
+    public void setCriticalValue(int criticalValue) {
+        this.criticalValue = criticalValue;
+    }
 }
