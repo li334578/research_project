c552734b04dd657b838a3db4f60e0941ce8d2536
liwenbo <liwenbo4922@163.com>
2022-06-03 23:35:27 +0800

    ConcurrentHashMapController

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/ConcurrentHashMapController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/ConcurrentHashMapController.java
@@ -8,4 +8,13 @@ package com.company.micro_service_1.controller;
  * @Created by 李文博
  */
 public class ConcurrentHashMapController {
+
+    // 线程数
+    private static final int THREAD_COUNT = 10;
+
+    // 元素个数
+    private static final int ITEM_COUNT = 1000;
+
+    //循环次数
+    private static final int LOOP_COUNT = 10000000;
 }
