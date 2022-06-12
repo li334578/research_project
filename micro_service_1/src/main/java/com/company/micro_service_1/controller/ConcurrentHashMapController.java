0a0e21f5a7cf0189b1bbad4a487bb1ce06fca2cb
liwenbo <liwenbo4922@163.com>
2022-06-12 19:58:40 +0800

    ConcurrentHashMapController

--- micro_service_1/src/main/java/com/company/micro_service_1/controller/ConcurrentHashMapController.java
+++ micro_service_1/src/main/java/com/company/micro_service_1/controller/ConcurrentHashMapController.java
@@ -107,6 +107,13 @@ public class ConcurrentHashMapController {
 
     @GetMapping("/test3")
     public String testConcurrentHashMap3() throws InterruptedException {
+        /*
+         * 使用 ConcurrentHashMap 来统计，Key 的范围是 10。
+         * 使用最多 10 个并发，循环操作 1000 万次，每次操作累加随机的 Key。
+         * 如果 Key 不存在的话，首次设置值为 1。
+         *
+         */
+        long start = System.currentTimeMillis();
         return "";
     }
 }
