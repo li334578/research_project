9ccca22847dc48b02d5d89619d30ae39c56313f1
liwenbo <liwenbo4922@163.com>
2022-07-13 20:53:12 +0800

    ProductService

---
+++ micro_service_1/src/main/java/com/company/micro_service_1/service/ProductService.java
@@ -0,0 +1,56 @@
+package com.company.micro_service_1.service;
+
+import com.company.micro_service_1.bean.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ * (Product)表服务接口
+ *
+ * @author makejava
+ * @since 2022-07-09 11:25:06
+ */
+public interface ProductService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    Product queryById(Long id);
+
+    /**
+     * 分页查询
+     *
+     * @param product     筛选条件
+     * @param pageRequest 分页对象
+     * @return 查询结果
+     */
+    Page<Product> queryByPage(Product product, PageRequest pageRequest);
+
+    /**
+     * 新增数据
+     *
+     * @param product 实例对象
+     * @return 实例对象
+     */
+    Product insert(Product product);
+
+    /**
+     * 修改数据
+     *
+     * @param product 实例对象
+     * @return 实例对象
+     */
+    Product update(Product product);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Long id);
+
+}
