b6b8b02851c5f0a47a10b56cb44d0c3902df26cd
liwenbo <liwenbo4922@163.com>
2022-08-01 13:55:27 +0800

    TestClass02

---
+++ micro_service_1/src/test/java/TestClass02.java
@@ -0,0 +1,31 @@
+import cn.hutool.bloomfilter.BitMapBloomFilter;
+import cn.hutool.core.util.NumberUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Date 1/8/2022 0001 下午 1:54
+ * @Description 测试类
+ * @Version 1.0.0
+ * @Author liwenbo
+ */
+@Slf4j
+public class TestClass02 {
+
+    @Test
+    public void testMethod1() {
+        BitMapBloomFilter bitMapBloomFilter = new BitMapBloomFilter(10);
+        bitMapBloomFilter.add("aaa");
+        bitMapBloomFilter.add("ccc");
+        bitMapBloomFilter.add("bbb");
+        // 查找
+        boolean abc = bitMapBloomFilter.contains("abc");
+
+
+        long mNum = NumberUtil.div(String.valueOf(10), String.valueOf(5)).longValue();
+        long size = mNum * 1024L * 1024L * 8L;
+
+        System.out.println(mNum);
+        System.out.println(size);
+    }
+}
