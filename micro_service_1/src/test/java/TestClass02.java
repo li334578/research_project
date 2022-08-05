d38ca2e0731ef996c9fa038e372f1916afa8515d
liwenbo <liwenbo4922@163.com>
2022-08-05 09:21:50 +0800

    TestClass02

--- micro_service_1/src/test/java/TestClass02.java
+++ micro_service_1/src/test/java/TestClass02.java
@@ -1,7 +1,14 @@
 import cn.hutool.bloomfilter.BitMapBloomFilter;
 import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.Digester;
+import com.company.micro_service_1.bean.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Date 1/8/2022 0001 下午 1:54
@@ -41,9 +48,25 @@ public class TestClass02 {
 
     @Test
     public void testMethod3() {
-        Student student = new Student(1L, "张三", 18);
-        Student clone = ObjectUtil.clone(student);
-        System.out.println(student.hashCode());
-        System.out.println(clone.hashCode());
+//        Student student = new Student(1L, "张三", 18);
+//        Student clone = ObjectUtil.clone(student);
+//        System.out.println(student.hashCode());
+//        System.out.println(clone.hashCode());
+    }
+
+    @Test
+    public void testMethod4() {
+        String username = "lwb008";
+        int index = Math.abs(username.hashCode()) % username.length();
+        System.out.println(index);
+
+        Digester md5 = new Digester(DigestAlgorithm.MD5);
+        Digester md52 = new Digester(DigestAlgorithm.MD5);
+        String c = String.valueOf(username.charAt(index));
+        md5.setSalt(c.getBytes(StandardCharsets.UTF_8));
+        String hex = md5.digestHex("123456");
+        String hex2 = md52.digestHex("123456");
+        System.out.println(hex);
+        System.out.println(hex2);
     }
 }
