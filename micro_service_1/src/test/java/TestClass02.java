ffa10b433775484ae8cfe78d43cb0b4ec4139cdf
liwenbo <liwenbo4922@163.com>
2022-08-08 14:15:59 +0800

    TestClass02

--- micro_service_1/src/test/java/TestClass02.java
+++ micro_service_1/src/test/java/TestClass02.java
@@ -8,7 +8,11 @@ import com.company.micro_service_1.bean.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
 
 /**
  * @Date 1/8/2022 0001 下午 1:54
@@ -68,5 +72,54 @@ public class TestClass02 {
         String hex2 = md52.digestHex("123456");
         System.out.println(hex);
         System.out.println(hex2);
+    }
+
+
+    @Test
+    public void testMethod5() throws Exception {
+        Map<Integer, Integer> map = new HashMap<>(7, 0.7f);
+        // 通过反射获取容量变量capacity,并调用map对象
+        Method capacity = map.getClass().getDeclaredMethod("capacity");
+        capacity.setAccessible(true);
+        System.out.println(capacity.invoke(map));
+        map.put(1, 1);
+        map.put(2, 2);
+        map.put(3, 3);
+        map.put(4, 4);
+        map.put(5, 5);
+        System.out.println(capacity.invoke(map));
+        map.put(6, 6);
+        System.out.println(capacity.invoke(map));
+        map.put(7, 7);
+        System.out.println(capacity.invoke(map));
+        // 三大方法
+        Executors.newFixedThreadPool(5);
+        Executors.newCachedThreadPool();
+        Executors.newSingleThreadExecutor();
+        // 七大参数 核心线程数 最大线程数 空闲线程存活时间 存活时间的单位 创建线程的工厂 存放任务的队列 拒绝策略
+
+        // 四种拒绝策略 disCardPolicy 静默拒绝 disOldestPolicy 丢弃队首 尝试把当前任务加进去
+        // abortPolicy 直接拒绝 抛出异常 callerRunsPolicy 调用人执行
+
+
+        // callerRunsPolicy 调用者执行
+        // disCardPolicy 静默拒绝
+        // disOldestPolicy 把队首任务丢弃 新任务加入
+        // abortPolicy 直接拒绝 并抛出异常 (RejectExecutionException)
+
+        // disCardPolicy 静默拒绝
+        // disOldestPolicy 丢弃队首任务 将新任务加入队尾
+        // abortPolicy 直接拒绝 抛出RejectExecutionException异常
+        // callerRunsPolicy 调用线程执行该任务
+
+        // 三次握手
+
+        // a -> seq=x b
+
+        // a seq=y ack=x+1 <- b
+
+        // a ack=y+1 -> b
+
+
     }
 }
