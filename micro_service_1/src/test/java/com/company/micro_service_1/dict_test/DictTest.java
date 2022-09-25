c405564a453387729b1f86209d9d0ab4663b48a8
liwenbo <liwenbo4922@163.com>
2022-09-26 07:35:04 +0800

    ansj 分析epub

--- micro_service_1/src/test/java/com/company/micro_service_1/dict_test/DictTest.java
+++ micro_service_1/src/test/java/com/company/micro_service_1/dict_test/DictTest.java
@@ -1,12 +1,21 @@
 package com.company.micro_service_1.dict_test;
 
+import cn.hutool.core.util.ReUtil;
+import lombok.extern.slf4j.Slf4j;
+import nl.siegmann.epublib.domain.*;
+import nl.siegmann.epublib.epub.EpubReader;
 import org.ansj.app.keyword.KeyWordComputer;
 import org.ansj.app.keyword.Keyword;
 import org.ansj.splitWord.analysis.ToAnalysis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @ClassName DictTest
@@ -15,16 +24,97 @@ import java.util.Collection;
  * @Date 25/9/2022 下午 6:27
  * @Version 1.0
  */
+@Slf4j
 public class DictTest {
     @Test
     public void testMethod1() {
-
-        String str = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!" ;
+        String str = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!";
         System.out.println(ToAnalysis.parse(str));
         KeyWordComputer kwc = new KeyWordComputer(5);
         String title = "维基解密否认斯诺登接受委内瑞拉庇护";
         String content = "有俄罗斯国会议员，9号在社交网站推特表示，美国中情局前雇员斯诺登，已经接受委内瑞拉的庇护，不过推文在发布几分钟后随即删除。俄罗斯当局拒绝发表评论，而一直协助斯诺登的维基解密否认他将投靠委内瑞拉。　　俄罗斯国会国际事务委员会主席普什科夫，在个人推特率先披露斯诺登已接受委内瑞拉的庇护建议，令外界以为斯诺登的动向终于有新进展。　　不过推文在几分钟内旋即被删除，普什科夫澄清他是看到俄罗斯国营电视台的新闻才这样说，而电视台已经作出否认，称普什科夫是误解了新闻内容。　　委内瑞拉驻莫斯科大使馆、俄罗斯总统府发言人、以及外交部都拒绝发表评论。而维基解密就否认斯诺登已正式接受委内瑞拉的庇护，说会在适当时间公布有关决定。　　斯诺登相信目前还在莫斯科谢列梅捷沃机场，已滞留两个多星期。他早前向约20个国家提交庇护申请，委内瑞拉、尼加拉瓜和玻利维亚，先后表示答应，不过斯诺登还没作出决定。　　而另一场外交风波，玻利维亚总统莫拉莱斯的专机上星期被欧洲多国以怀疑斯诺登在机上为由拒绝过境事件，涉事国家之一的西班牙突然转口风，外长马加略]号表示愿意就任何误解致歉，但强调当时当局没有关闭领空或不许专机降落。";
         Collection<Keyword> result = kwc.computeArticleTfidf(title, content);
         System.out.println(result);
+    }
+
+    @Test
+    public void testMethod2() {
+        KeyWordComputer kwc = new KeyWordComputer(100);
+        List<Keyword> list = kwc.computeArticleTfidf(getContent("E:\\download\\Documents\\庆余年.epub"));
+        for (Keyword keyword : list) {
+            log.info(keyword.toString());
+        }
+    }
+
+    public String getContent(String path) {
+        File file = new File(path);
+        InputStream in = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            //从输入流当中读取epub格式文件
+            EpubReader reader = new EpubReader();
+            in = new FileInputStream(file);
+            Book book = reader.readEpub(in);
+            //获取到书本的头部信息
+            Metadata metadata = book.getMetadata();
+            System.out.println("FirstTitle为：" + metadata.getFirstTitle());
+            //获取到书本的全部资源
+            Resources resources = book.getResources();
+            System.out.println("所有资源数量为：" + resources.size());
+            //获取所有的资源数据
+            Collection<String> allHrefs = resources.getAllHrefs();
+            for (String href : allHrefs) {
+                Resource resource = resources.getByHref(href);
+                //data就是资源的内容数据，可能是css,html,图片等等
+                byte[] data = resource.getData();
+                // 获取到内容的类型  css,html,还是图片
+                MediaType mediaType = resource.getMediaType();
+            }
+            //获取到书本的内容资源
+            List<Resource> contents = book.getContents();
+            System.out.println("内容资源数量为：" + contents.size());
+            //获取到书本的spine资源 线性排序
+            Spine spine = book.getSpine();
+            System.out.println("spine资源数量为：" + spine.size());
+            //通过spine获取所有的数据
+            List<SpineReference> spineReferences = spine.getSpineReferences();
+            for (SpineReference spineReference : spineReferences) {
+                Resource resource = spineReference.getResource();
+                //data就是资源的内容数据，可能是css,html,图片等等
+                byte[] data = resource.getData();
+                String s = new String(data);
+//                String s1 = ReUtil.replaceAll(s, "[!0-9a-zA-Z<>=\"-/“”。,，.— ──:;?{}]：", "");
+                stringBuilder.append(s);
+                // 获取到内容的类型  css,html,还是图片
+                MediaType mediaType = resource.getMediaType();
+            }
+            //获取到书本的目录资源
+            TableOfContents tableOfContents = book.getTableOfContents();
+            System.out.println("目录资源数量为：" + tableOfContents.size());
+            //获取到目录对应的资源数据
+            List<TOCReference> tocReferences = tableOfContents.getTocReferences();
+            for (TOCReference tocReference : tocReferences) {
+                Resource resource = tocReference.getResource();
+                //data就是资源的内容数据，可能是css,html,图片等等
+                byte[] data = resource.getData();
+                // 获取到内容的类型  css,html,还是图片
+                MediaType mediaType = resource.getMediaType();
+                if (tocReference.getChildren().size() > 0) {
+                    //获取子目录的内容
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //一定要关闭资源
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return stringBuilder.toString();
     }
 }
