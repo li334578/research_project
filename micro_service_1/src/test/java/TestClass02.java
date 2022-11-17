457ec4554bbae01baf8d962aa12fd7effbf30423
liwenbo <liwenbo4922@163.com>
2022-11-17 09:24:43 +0800

    testMethod

--- micro_service_1/src/test/java/TestClass02.java
+++ micro_service_1/src/test/java/TestClass02.java
@@ -10,7 +10,10 @@ import cn.hutool.cron.timingwheel.TimerTaskList;
 import cn.hutool.cron.timingwheel.TimingWheel;
 import cn.hutool.crypto.digest.DigestAlgorithm;
 import cn.hutool.crypto.digest.Digester;
-import com.baomidou.mybatisplus.extension.exceptions.ApiException;
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
@@ -515,6 +518,7 @@ public class TestClass02 {
     public void testMethod26() {
         StampedLock stampedLock = new StampedLock();
     }
+
     /**
      * 给你一个长度固定的整数数组 arr，请你将该数组中出现的每个零都复写一遍，并将其余的元素向右平移。
      * <p>
@@ -643,7 +647,6 @@ public class TestClass02 {
 //    }
 
 
-
     @Test
     public void testMethod29() {
         @Data
@@ -736,6 +739,41 @@ public class TestClass02 {
     public void testMethod33() {
         System.out.println(39 / 20 + 1);
     }
+
+    @Test
+    public void testMethod34() throws com.taobao.api.ApiException {
+        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=token");
+        OapiRobotSendRequest request = new OapiRobotSendRequest();
+        request.setMsgtype("text");
+        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+        text.setContent("测试文本消息 小钉");
+        request.setText(text);
+        OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+        at.setAtMobiles(Arrays.asList("手机号"));
+// isAtAll类型如果不为Boolean，请升级至最新SDK
+        at.setIsAtAll(true);
+//        at.setAtUserIds(Arrays.asList("109929", "32099"));
+        request.setAt(at);
+
+        request.setMsgtype("link");
+        OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
+        link.setMessageUrl("https://www.dingtalk.com/");
+        link.setPicUrl("");
+        link.setTitle("时代的火车向前开");
+        link.setText("这个即将发布的新版本，创始人小钉称它为红树林。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是红树林");
+        request.setLink(link);
+
+        request.setMsgtype("markdown");
+        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+        markdown.setTitle("杭州天气");
+        markdown.setText("#### 杭州天气 @18737843824\n" +
+                "> 9度，西北风1级，空气良89，相对温度73%\n\n" +
+                "> 小钉 ![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)\n" +
+                "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n");
+        request.setMarkdown(markdown);
+        OapiRobotSendResponse response = client.execute(request);
+    }
+
 
     @Test
     public void testMethod35() {
