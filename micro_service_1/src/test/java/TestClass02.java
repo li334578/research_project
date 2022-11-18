aadd33e6ad6cbe23a2a2dfe31ab2aa7ffd7a5269
liwenbo <liwenbo4922@163.com>
2022-11-18 15:33:01 +0800

    testMethod

--- micro_service_1/src/test/java/TestClass02.java
+++ micro_service_1/src/test/java/TestClass02.java
@@ -18,6 +18,8 @@ import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.icepear.echarts.Bar;
+import org.icepear.echarts.render.Engine;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -788,4 +790,20 @@ public class TestClass02 {
         log.info("A result is :" + a);
         log.info("B result is :" + b);
     }
+    @Test
+    public void testMethod36() {
+        Bar bar = new Bar()
+                .setLegend()
+                .setTooltip("item")
+                .addXAxis(new String[]{"Matcha Latte", "Milk Tea", "Cheese Cocoa", "Walnut Brownie"})
+                .addYAxis()
+                .addSeries("2015", new Number[]{43.3, 83.1, 86.4, 72.4})
+                .addSeries("2016", new Number[]{85.8, 73.4, 65.2, 53.9})
+                .addSeries("2017", new Number[]{93.7, 55.1, 82.5, 39.1});
+        Engine engine = new Engine();
+        // The render method will generate our EChart into a HTML file saved locally in the current directory.
+        // The name of the HTML can also be set by the first parameter of the function.
+        engine.render("index.html", bar);
+    }
+
 }
