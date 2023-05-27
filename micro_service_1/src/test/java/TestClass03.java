bfbaf871f25847952791238716d2e3c7089bc5f8
liwenbo <liwenbo4922@163.com>
2023-05-27 14:36:24 +0800

    testMethod21

--- micro_service_1/src/test/java/TestClass03.java
+++ micro_service_1/src/test/java/TestClass03.java
@@ -602,4 +602,71 @@ public class TestClass03 {
         loadingCache.getUnchecked("key1");
     }
 
+    @Test
+    public void testMethod24() {
+        // weigher 设置权重值 如果所有缓存的key的权重之和大于了我们指定的最大权重，那么将执行LRU淘汰策略：
+        LoadingCache<String, String> loadingCache = CacheBuilder.newBuilder().maximumWeight(6).weigher((key, value) -> {
+                    if (key.equals("key1")) {
+                        return 1;
+                    }
+                    if (key.equals("key2")) {
+                        return 2;
+                    }
+                    if (key.equals("key3")) {
+                        return 3;
+                    }
+
+                    if (key.equals("key4")) {
+                        return 1;
+                    }
+                    return 0;
+                })
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) {
+                        System.out.println(key + "真正计算了");
+                        return "cached-" + key;
+                    }
+                });
+
+        System.out.println("第一次访问");
+        loadingCache.getUnchecked("key1");
+        loadingCache.getUnchecked("key2");
+        loadingCache.getUnchecked("key3");
+
+        System.out.println("第二次访问");
+        loadingCache.getUnchecked("key1");
+        loadingCache.getUnchecked("key2");
+        loadingCache.getUnchecked("key3");
+
+        System.out.println("开始剔除");
+        loadingCache.getUnchecked("key4");
+        loadingCache.getUnchecked("key3");
+        loadingCache.getUnchecked("key2");
+        loadingCache.getUnchecked("key1");
+    }
+
+    @Test
+    public void testMethod25() throws Exception {
+        // expireAfterAccess 当某个缓存key自最后一次访问（读取或者写入）超过指定时间后，那么这个缓存key将失效。
+        // expireAfterWrite 这个方法只关心写入
+        LoadingCache<String, String> loadingCache = CacheBuilder.newBuilder().expireAfterAccess(3, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) {
+                        System.out.println(key + "真正计算了");
+                        return "cached-" + key;
+                    }
+                });
+
+        System.out.println("第一次访问（写入）");
+        loadingCache.getUnchecked("key1");
+
+        System.out.println("第二次访问");
+        loadingCache.getUnchecked("key1");
+
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("过3秒后访问");
+        loadingCache.getUnchecked("key1");
+    }
 }
